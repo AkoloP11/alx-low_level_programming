82ef1c1bbb4e4842d738c13d935e3f7046bc8bf0
Akolo Promise <akolopromise86@gmail.com>
2023-09-08 07:28:14 -0700

    Print Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 97; b <= 122; b++)
+	{
+		putchar(b);
+	}
+	for (b = 65; b <= 90; b++)
+	{
+		putchar(b);
+	}
+		putchar('\n');
+
+		return (0);
+}
