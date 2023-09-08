7eba3d6300548872e004654dc076c8ac822c536b
Akolo Promise <akolopromise86@gmail.com>
2023-09-08 08:00:32 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Return: Always 0(Success)
+ */
+int  main(void)
+{
+	int i;
+
+	for (i = 30; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
