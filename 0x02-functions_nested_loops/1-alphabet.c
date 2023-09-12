2718dda02d73d54d15563e9e61f95de4778ec8df
Akolo Promise <akolopromise86@gmail.com>
2023-09-12 05:45:20 -0700

    Printing the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * Print_alphabet - prints alphabets
+ *
+ *
+ * Return: Always 0(Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return 0;
+}
