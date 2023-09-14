011c076b4bc760ba3c4470b19747818efa730aea
Akolo Promise <akolopromise86@gmail.com>
2023-09-14 06:37:20 -0700

    DIGITS

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - Detects if a character is upper or lowercase
+ * @c: character to be checked
+ * Return: 1 if uppercase, 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
