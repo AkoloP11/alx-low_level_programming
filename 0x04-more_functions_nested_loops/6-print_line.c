2b30dcd1aae4c3f9cb2e4a48b732e30154b5d5f9
Akolo Promise <akolopromise86@gmail.com>
2023-09-14 13:58:37 -0700

    Straight lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * print_line - Draws a straight line in the terminal
  * @n: The number of times an underscore is printed
  */
 void print_line(int n)
 {
-	int c = n = '46';
-
-	_putchar(n);
+	int c;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		c = 1;
+		while (c <= n)
+		{
+		putchar('_');
+		c++;
+		}
+	}
+	putchar('\n');
 }
